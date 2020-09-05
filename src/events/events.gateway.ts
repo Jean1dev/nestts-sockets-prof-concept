8023e6b6c199902e9e22844240a8d2f0c9a05329
Jean <jeanlucafp@gmail.com>
2020-09-05 20:43:25 -0300

    feito tutorial no medium

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,21 +1,45 @@
 import {
-    MessageBody,
-    SubscribeMessage,
-    WebSocketGateway,
-    WebSocketServer,
-    WsResponse,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets'
 
 import { Server } from 'socket.io'
 
 @WebSocketGateway()
-export default class EventsGateway {
+export default class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
-    @WebSocketServer()
-    server: Server;
+  private wsClients = []
 
-    @SubscribeMessage('identity')
-    async identity(@MessageBody() data: number): Promise<number> {
-      return data;
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('identity')
+  async identity(@MessageBody() data: any): Promise<number> {
+    return data
+  }
+
+  public handleConnection(client: any, ...args: any[]) {
+    this.wsClients.push(client)
+  }
+
+  public handleDisconnect(client: any) {
+    for (let i = 0; i < this.wsClients.length; i++) {
+      if (this.wsClients[i] === client) {
+        this.wsClients.splice(i, 1)
+        break
+      }
     }
+  }
+
+  private broadcast(event, broadCastMessage: any, userId: string) {
+    this.wsClients.filter(socket => {
+      return socket.handshake.query.userId == userId
+    }).forEach(socket => {
+      socket.emit(event, broadCastMessage)
+    })
+  }
 }
